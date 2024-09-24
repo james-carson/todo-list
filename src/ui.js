0c65f9cb9903fd48a5578c8a1a9efc96f7436972
carsoninho <james.87.carson@gmail.com>
2024-09-24 17:51:21 +0700

    Checkpoint - need to add logic for UI

---
+++ src/ui.js
@@ -0,0 +1,30 @@
+import { loadData, saveData } from './storage';
+import { Todo } from './todo';
+import { Projects } from './project';
+import { isToday, isThisWeek } from 'date-fns';
+
+// Filter lists by date
+
+
+
+// Filter lists for high priority
+
+
+
+// Filter for completed
+
+
+
+// Get names of all lists
+
+
+
+// Render all in order
+
+
+
+// Add event listeners for clicks
+
+
+
+// Logic for displaying todos on content
