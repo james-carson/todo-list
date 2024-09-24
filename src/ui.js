85d8ae10bd32928d09a5239658ec45782cb8e95d
carsoninho <james.87.carson@gmail.com>
2024-09-24 19:18:37 +0700

    Checkpoint - in the middle of ui
    Need to add event listeners and rendering functions

--- src/ui.js
+++ src/ui.js
@@ -3,24 +3,40 @@ import { Todo } from './todo';
 import { Projects } from './project';
 import { isToday, isThisWeek } from 'date-fns';
 
-// Filter lists by date
+// Filter lists by date (and uncompleted)
 
+function getTodosDueToday(todos) {
+    return todos.filter(todo => {
+        // Convert dueDate to Date object:
+        const dueDate = new Date(todo.dueDate);
+        // Check if the todo is due today and is also uncompleted:
+        return isToday(dueDate) && !todo.completed;
+    });
+}
 
+// Filter lists for high priority (and uncompleted)
 
-// Filter lists for high priority
-
-
+// Same as above, but with this week rather than today:
+function getTodosDueThisWeek(todos) {
+    return todos.filter(todo => {
+        const dueDate = new Date(todo.dueDate);
+        return isThisWeek(dueDate) && !todo.completed;
+    });
+}
 
 // Filter for completed
 
-
+function getCompletedTodos(todos) {
+    return todos.filter(todo => todo.complete);
+}
 
 // Get names of all lists
 
-
+function getProjectNames(projects) {
+    return projects.map(project => project.name);
+}
 
 // Render all in order
-
 
 
 // Add event listeners for clicks
