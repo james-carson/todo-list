01ea6a8d06880187435c1222fdd322c0ed0ea10d
carsoninho <james.87.carson@gmail.com>
2024-09-24 13:20:48 +0700

    Added methods to Todos

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,7 @@
 class Todo {
+
+// Todo item constructor:
+
     constructor(title, dueDate, priority, notes, complete) {
         this.title = title
         this.dueDate = dueDate
@@ -6,6 +9,22 @@ class Todo {
         this.notes = notes
         this.complete = complete
     }
+
+// Method to edit items:
+
+    editTodo(newTitle, newDueDate, newPriority, newNotes) {
+        this.title = newTitle || this.title;
+        this.dueDate = newDueDate || this.dueDate;
+        this.priority = newPriority || this.priority;
+        this.notes = newNotes || this.notes;
 }
 
-// Create, edit, complete, delete
+// Method to toggle todo as complete/incomplete:
+
+toggleComplete() {
+    this.complete = !this.complete;
+}
+
+// Deleting will be done through Projects
+
+}
