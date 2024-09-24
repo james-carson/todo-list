2bae9d89d21f42a7445aa0efe70df65a70248316
carsoninho <james.87.carson@gmail.com>
2024-09-24 11:41:38 +0700

    Setup Class constructors for todos and projects

---
+++ src/project.js
@@ -0,0 +1,8 @@
+class Project {
+    constructor(name, todoList) {
+        this.name = name
+        this.todoList = todoList
+    }
+}
+
+// Add, view, delete?
---
+++ src/todo.js
@@ -0,0 +1,11 @@
+class Todo {
+    constructor(title, dueDate, priority, notes, complete) {
+        this.title = title
+        this.dueDate = dueDate
+        this.priority = priority
+        this.notes = notes
+        this.complete = complete
+    }
+}
+
+// Create, edit, complete, delete
